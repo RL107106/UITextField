3ad2facf07a76bb3ff4b75b703e63e661dbe8ee9
NATHAN GEHRKE <rl107106@rlas-macair-22.local>
2018-12-11 11:36:19 -0600

    Initial Commit

---
+++ UITextField/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UITextField
+//
+//  Created by NATHAN GEHRKE on 10/4/18.
+//  Copyright © 2018 Nathan Gehrke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var greetingsAppLabel: UILabel!
+    @IBOutlet weak var enterNametextField: UITextField!
+    @IBAction func sayNameButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+
+
+}
+
+}
